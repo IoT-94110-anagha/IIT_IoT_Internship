c1e5fe32bcfb2d89be5a71a71e4baeaca0352488
patilanagha2601 <patilanagha2601@gmail.com>
2025-12-12 13:11:31 +0530

    Assignment 1 push

---
+++ assignment 1/que1.py
@@ -0,0 +1,3 @@
+name=input("enter name=")
+age=input("enter age=")
+print("your name is",name ,"and your age is",age)
---
+++ assignment 1/que4.py
@@ -0,0 +1,7 @@
+a=int(input("enter 1st number="))
+b=int(input("enter 2nd number="))
+c=a+b
+s=a-b
+m=a*b
+d=a/b
+print("result","\naddition:",c,"\nsubstraction:",s,"\nmultiplication:",m,"\ndivision:",d)
